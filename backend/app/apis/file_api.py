689f492035ed80de7993a3b865ce7e065318cb7e
rosenari <rosenari88@gmail.com>
2024-10-11 20:57:28 +0900

    fast api Dockerfile 작성

--- backend/app/apis/file_api.py
+++ backend/app/apis/file_api.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, UploadFile, HTTPException, Depends
+from fastapi import APIRouter, UploadFile, HTTPException
 from typing import List
 from app.services.file_service import FileService
 

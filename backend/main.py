38fbf31bfa4196f6856792c67adf4a13a490cf71
rosenari92 <rosenari92@gmail.com>
2024-10-12 00:50:43 +0900

    file api 수정 및 dashboard 파일 업로드 구현완료

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
 from app.apis import file_api
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 허용할 출처 목록
+    allow_credentials=True,  # 자격 증명(쿠키 등) 허용 여부
+    allow_methods=["*"],  # 모든 HTTP 메서드 허용 (GET, POST, PUT 등)
+    allow_headers=["*"],  # 모든 헤더 허용
+)
 
 # 파일 관련 API 라우터 등록
 app.include_router(file_api.router, prefix="/files")

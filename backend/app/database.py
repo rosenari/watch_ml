3e7524aa18708de25f325ba87b016191f2208c67
rosenari <rosenari88@gmail.com>
2024-10-13 13:21:13 +0900

    redis test 작성

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,9 +1,10 @@
+import os
 from redis.asyncio import from_url
 
 
 async def get_redis():
-    redis = await from_url("redis://redis:6379/0")
-    redis.exists
+    redis = await from_url(os.environ.get('CELERY_BROKER_URL', 'redis://redis:6379/0'))
+
     try:
         yield redis
     finally:

d16b1bb66f92445fa5230d77450b26d4dc958b99
rosenari <rosenari88@gmail.com>
2024-10-29 12:53:03 +0900

    ml_service, create_model 버그 수정

--- backend/app/services/ml_service.py
+++ backend/app/services/ml_service.py
@@ -16,7 +16,7 @@ class MlService:
     @transactional
     async def init_model(self, model_name: str, base_model_name: str = None) -> int:
         model = await self.repository.get_model_by_name(model_name)
-        version = 1 if model is None else model['version'] + 1
+        version = 1 if model is None else (model.version if model.status == Status.FAILED else model.version + 1)
         await self.register_model(AiModelDTO(
             model_name=model_name, version=version, base_model_name=base_model_name
         ))
